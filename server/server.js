ccf29745da17ac2721eee25711b5a469a95163c6
Alberto <alapizco29@gmail.com>
2020-06-18 22:24:55 -0600

    Se sube ejemplo inicial

---
+++ server/server.js
@@ -0,0 +1,54 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.json('Hello World');
+});
+
+app.get('/usuario', (req, res) => {
+    res.json('petición get');
+});
+
+app.post('/usuario', (req, res) => {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+
+});
+
+app.put('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    res.json({
+        ok: true,
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('petición delete');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando puerto: ", process.env.PORT);
+});
