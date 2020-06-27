c1065f1e0a40fa9805e67647aaa100f17fccb948
Alberto <alapizco29@gmail.com>
2020-06-26 23:23:43 -0600

    Se agrega variable de entorno para tomar las credenciales de mogodb

--- server/config/config.js
+++ server/config/config.js
@@ -6,10 +6,12 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // url
-let conexion = 'mongodb+srv://kittcho:LSqKeOmMhc3HEAYa@cluster0-hbpuf.mongodb.net/cafe';
+let conexion = '';
 
 if (process.env.NODE_ENV === 'dev') {
     conexion = 'mongodb://localhost:27017/cafe';
+} else {
+    conexion = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = conexion;
